3c1d6aaa382c6c8f8193ce49df9e8432bc8c74f0
vladchobotok <47303070+vladchobotok@users.noreply.github.com>
2021-10-17 17:59:21 +0300

    Added Homework 3

---
+++ OOP-Homework-3/Task3/Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(27.32, 30.82);
+            Console.WriteLine(converter.HrnToUsd(3000));
+            Console.WriteLine(converter.UsdToHrn(30));
+            Console.WriteLine(converter.HrnToEur(3000));
+            Console.WriteLine(converter.EurToHrn(30));
+        }
+    }
+
+    public class Converter
+    {
+        public double eur { get; set; }
+        public double usd { get; set; }
+
+        public Converter(double _usd, double _eur)
+        {
+            usd = _usd;
+            eur = _eur;
+        }
+
+        public double HrnToUsd(double hrnAmount)
+        {
+            return hrnAmount / usd;
+        }
+
+        public double UsdToHrn(double usdAmount)
+        {
+            return usdAmount * usd;
+        }
+
+        public double HrnToEur(double hrnAmount)
+        {
+            return hrnAmount / eur;
+        }
+
+        public double EurToHrn(double eurAmount)
+        {
+            return eurAmount * eur;
+        }
+    }
+}
